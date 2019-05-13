e72ac6be84d5aa379bb59d29f09f3930c9dfc898
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-13 19:33:22 +0300

    done part 1 and 2

--- Constraints01/ViewController.swift
+++ Constraints01/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     var square: UIView = {
         let myView = UIView()
         myView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        myView.backgroundColor = .blue
         myView.translatesAutoresizingMaskIntoConstraints = false
         
         return myView
@@ -12,11 +13,82 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(square)
+        
+        
+//        createSquareWithConstraintsCenter()
+
+        createSquareWithConstraints()
+
+    }
+    
+    func createSquareWithConstraints() {
+        NSLayoutConstraint(item: square,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: square,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+         NSLayoutConstraint(item: square,
+                            attribute: .topMargin,
+                            relatedBy: .equal,
+                            toItem: self.view,
+                            attribute: .topMargin,
+                            multiplier: 1,
+                            constant: 20).isActive = true
+        
+         NSLayoutConstraint(item: square,
+                            attribute: .height,
+                            relatedBy: .equal,
+                            toItem: square,
+                            attribute: .width,
+                            multiplier: 1,
+                            constant: 0).isActive = true
     }
     
     
-    func createSquareWithConstraints() {
+    func createSquareWithConstraintsCenter() {
+        NSLayoutConstraint(item: square,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .centerX,
+                           multiplier: 1,
+                           constant: 0).isActive = true
         
+        NSLayoutConstraint(item: square,
+                           attribute: .centerY,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .centerY,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: square,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: nil,
+                           attribute: .notAnAttribute,
+                           multiplier: 1,
+                           constant: 200).isActive = true
+        
+        NSLayoutConstraint(item: square,
+                           attribute: .width,
+                           relatedBy: .equal,
+                           toItem: nil,
+                           attribute: .notAnAttribute,
+                           multiplier: 1,
+                           constant: 200).isActive = true
     }
     
 }
